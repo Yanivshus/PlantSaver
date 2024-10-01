ba09169b047efab770b72449792fd0ba11ff5774
Yanivshus <yanivshus@gmail.com>
2024-10-01 16:24:22 +0300

    can get results

--- Back+Front/backend/StatusApp/process.py
+++ Back+Front/backend/StatusApp/process.py
@@ -1,6 +1,19 @@
 from .models import Entry
 from math import isclose
 
+def mainProcessing(enteries):
+    score = 100
+
+    tempMsg, humidityMsg, envScr = checkPropperEnviroment(enteries)
+    score += envScr
+
+    waterMsg, waterScr = checkWaterLevel(enteries)
+    score += waterScr
+
+    msg = tempMsg + "|" + humidityMsg + "|" + waterMsg
+    return (score, msg)
+
+
 def checkPropperEnviroment(enteries):
     avgTemp = 0
     avgHumidity = 0
@@ -13,17 +26,22 @@ def checkPropperEnviroment(enteries):
             avgTemp += entry.temp
             avgHumidity += entry.humidity
 
+    score = 0
     if 20 <= avgTemp // len(enteries) and avgTemp // len(enteries) <= 30:
         tempMsg = f"Propper environment temperature, current avg: {avgTemp // len(enteries)}"
+        score = 0
     else:
         tempMsg = f"Not propper environment temperature: should keep between 20 and 30 degrees, current avg: {avgTemp // len(enteries)}"
+        score = -25
 
     if 60 <= avgHumidity // len(enteries) and avgHumidity // len(enteries) <= 80:
         humidityMsg = f"Propper environment humidity, current avg: {avgHumidity // len(enteries)}"
+        score = 0
     else:
         humidityMsg = f"Not propper environment humidity: should keep between 60 and 80 precent, current avg: {avgHumidity // len(enteries)}"
+        score = -25
 
-    return (tempMsg, humidityMsg)
+    return (tempMsg, humidityMsg, score)
 
 
 def checkWaterLevel(enteries):
@@ -35,5 +53,15 @@ def checkWaterLevel(enteries):
     amountEnteries = len(enteries)  
 
     supposedAmount = amountEnteries // 2
-    return isclose(enoughWater, supposedAmount, abs_tol=10) 
+    if enoughWater - 10 > supposedAmount :
+        return ("You are using too much water, try lowering it down", -25)
+    elif enoughWater + 10  < supposedAmount:
+        return ("You are not using enough water, try to pour more", -25)
+    else:
+        return ("You are providing enough water", 0)
+    
+
+
+    
+    
     
--- Back+Front/backend/StatusApp/views.py
+++ Back+Front/backend/StatusApp/views.py
@@ -7,6 +7,7 @@ from .serializers import EntrySerializer
 from .serializers import ResultSerializer
 from datetime import timedelta
 from django.utils import timezone
+from rest_framework.response import Response
 
 
 class EntryViewSet(ModelViewSet):
@@ -20,10 +21,89 @@ class ResultViewSet(ModelViewSet):
 
     def list(self, request):
         last_week = timezone.now() - timedelta(days=7)
-        entries = Entry.objects.filter(date=last_week)
+        entry_type = request.query_params.get('device_name', None)
+        entries = Entry.objects.filter(date__gte=last_week)
+
+        if entry_type:
+            entries = entries.filter(device_name=entry_type)
+
+        proc_data = self.process_and_save_results(entries)
+
+        return Response(proc_data)
+
+
 
     def process_and_save_results(self, enteries):
-        pass
+        score, msg = self.mainProcessing(enteries)
+
+        result = Result.objects.create(score=score, sentence= msg)
+
+        result_serializer = ResultSerializer(result)
+
+        return result_serializer.data
+    
+    def mainProcessing(self, enteries):
+        score = 100
+
+        tempMsg, humidityMsg, envScr = self.checkPropperEnviroment(enteries)
+        score += envScr
+
+        waterMsg, waterScr = self.checkWaterLevel(enteries)
+        score += waterScr
+
+        msg = tempMsg + "|" + humidityMsg + "|" + waterMsg
+        return (score, msg)
+
+
+    def checkPropperEnviroment(self,enteries):
+        avgTemp = 0
+        avgHumidity = 0
+        tempMsg = ""
+        humidityMsg = ""
+        for entry in enteries:
+            if entry.humidity < 0:
+                continue
+            else:
+                avgTemp += entry.temp
+                avgHumidity += entry.humidity
+
+        score = 0
+        if 20 <= avgTemp // len(enteries) and avgTemp // len(enteries) <= 30:
+            tempMsg = f"Propper environment temperature, current avg: {avgTemp // len(enteries)}"
+            score = 0
+        else:
+            tempMsg = f"Not propper environment temperature: should keep between 20 and 30 degrees, current avg: {avgTemp // len(enteries)}"
+            score = -25
+
+        if 60 <= avgHumidity // len(enteries) and avgHumidity // len(enteries) <= 80:
+            humidityMsg = f"Propper environment humidity, current avg: {avgHumidity // len(enteries)}"
+            score = 0
+        else:
+            humidityMsg = f"Not propper environment humidity: should keep between 60 and 80 precent, current avg: {avgHumidity // len(enteries)}"
+            score = -25
+
+        return (tempMsg, humidityMsg, score)
+
+
+    def checkWaterLevel(self, enteries):
+        enoughWater = 0
+        for entry in enteries:
+            if entry.hasWater:
+                enoughWater += 1
+
+        amountEnteries = len(enteries)  
+
+        supposedAmount = amountEnteries // 2
+        if enoughWater - 10 > supposedAmount :
+            return ("You are using too much water, try lowering it down", -25)
+        elif enoughWater + 10  < supposedAmount:
+            return ("You are not using enough water, try to pour more", -25)
+        else:
+            return ("You are providing enough water", 0)
+
+
+        
+        
 
     
 
